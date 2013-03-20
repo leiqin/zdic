bf524936f07aa97680c121ae6c973fa262255836
leiqin <leiqin2010@gmail.com>
2013-03-20 16:49:19 +0800

    * 设置选项时，如果选项和默认值相同就，就 clearUserPref

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -34,12 +34,69 @@ var zdicopts = {
 	setPref : function() {
 		var prefs = Components.classes["@mozilla.org/preferences-service;1"]
 			.getService(Components.interfaces.nsIPrefService);
-		var myprefs = prefs.getBranch("extensions.zdic.");
+		this._myprefs = prefs.getBranch("extensions.zdic.");
+		this._defprefs = prefs.getDefaultBranch("extensions.zdic.");
 
-		myprefs.setBoolPref("enableSelect", this.enableSelect);
-		myprefs.setBoolPref("enableCtrlHover", this.enableCtrlHover);
-		myprefs.setCharPref("ctrlHoverKey", this.ctrlHoverKey);
-		myprefs.setBoolPref("debug", this.debug);
+		this.setPrefValue("enableSelect", this.enableSelect);
+		this.setPrefValue("enableCtrlHover", this.enableCtrlHover);
+		this.setPrefValue("ctrlHoverKey", this.ctrlHoverKey);
+		this.setPrefValue("debug", this.debug);
+	},
+
+	_myprefs : null,
+	_defprefs : null,
+
+	setPrefValue : function(name, value, type) {
+		if (!type) {
+			vtype = typeof(value);
+			switch(vtype) {
+				case "string":
+					type = "char";
+					break;
+				case "number":
+					type = "int";
+					value = Math.round(value);
+					break;
+				case "boolean":
+					type = "bool";
+					break;
+			}
+		}
+		switch (type) {
+			case "char":
+				if (this._myprefs.getCharPref(name) == value) 
+					return;
+				if (this._defprefs.getCharPref(name) == value) {
+					if (this._myprefs.prefHasUserValue(name)) {
+						this._myprefs.clearUserPref(name);
+						return;
+					}
+				}
+				this._myprefs.setCharPref(name, value);
+				break;
+			case "bool":
+				if (this._myprefs.getBoolPref(name) == value) 
+					return;
+				if (this._defprefs.getBoolPref(name) == value) {
+					if (this._myprefs.prefHasUserValue(name)) {
+						this._myprefs.clearUserPref(name);
+						return;
+					}
+				}
+				this._myprefs.setBoolPref(name, value);
+				break;
+			case "int":
+				if (this._myprefs.getIntPref(name) == value) 
+					return;
+				if (this._defprefs.getIntPref(name) == value) {
+					if (this._myprefs.prefHasUserValue(name)) {
+						this._myprefs.clearUserPref(name);
+						return;
+					}
+				}
+				this._myprefs.setIntPref(name, value);
+				break;
+		}
 	},
 
 	readDialog : function() {
